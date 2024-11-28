3a11d4efded9f001f2c94ea960691d8bd97a4d64
Dhilipvelu <dhilipvelu25@gmail.com>
2024-11-28 15:51:19 +0530

    video change

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import explorevideo from "./images/video.mp4"
+import explorevideo from "./images/video1.mp4"
 import Mysidebar from "./Sidebar";
 
 function Myexplore(){
